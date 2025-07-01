62ff60ad6d42b115c54816330527fc4cf7e8d116
EyadZn <84345073+Eyad-Zanaty@users.noreply.github.com>
2025-07-01 19:44:58 +0300

    Add files via upload

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,14 @@
+SELECT users , count(price) as user_count FROM airbnb_table group by price;
+
+SELECT 
+    airbnb_table.property_name,
+    airbnb_table.total_bookings,
+    RANK() OVER (ORDER BY airbnb_table.total_bookings DESC) AS ranking
+FROM (
+    SELECT 
+        property_id,
+        COUNT(*) AS total_bookings
+    FROM bookings
+    GROUP BY property_id
+) AS booking_counts
+JOIN properties p ON airbnb_table.property_id = booking_counts.property_id;
