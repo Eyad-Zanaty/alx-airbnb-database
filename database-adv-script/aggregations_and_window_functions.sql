95b12420607e2e37bc3f2f1f520ad388cce85a62
EyadZn <84345073+Eyad-Zanaty@users.noreply.github.com>
2025-07-01 19:55:01 +0300

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -12,3 +12,19 @@ FROM (
     GROUP BY property_id
 ) AS booking_counts
 JOIN properties p ON airbnb_table.property_id = booking_counts.property_id;
+
+
+SELECT users , count(price) as user_count FROM airbnb_table group by price;
+
+SELECT 
+    airbnb_table.property_name,
+    airbnb_table.total_bookings,
+    ROW_NUMBER() OVER (ORDER BY airbnb_table.total_bookings DESC) AS ranking
+FROM (
+    SELECT 
+        property_id,
+        COUNT(*) AS total_bookings
+    FROM bookings
+    GROUP BY property_id
+) AS booking_counts
+JOIN properties p ON airbnb_table.property_id = booking_counts.property_id;
