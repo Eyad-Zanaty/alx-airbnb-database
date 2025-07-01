92ab0fbff5985cf190383be6e6f46a5790064828
EyadZn <84345073+Eyad-Zanaty@users.noreply.github.com>
2025-07-01 23:42:47 +0300

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,2 +1,30 @@
-SELECT bookings, user_details, property_details, payment_details
-FROM airbnb_table WHERE bookings > 0 AND user_details > " ";
+SELECT 
+    b.id AS booking_id,
+    u.id AS user_id,
+    u.username,
+    p.id AS property_id,
+    p.name AS property_name,
+    pay.id AS payment_id,
+    pay.amount,
+    pay.status
+FROM bookings b
+JOIN users u ON b.user_id = u.id
+JOIN properties p ON b.property_id = p.id
+JOIN payments pay ON b.payment_id = pay.id
+WHERE b.id > 0 AND u.username > ' ';
+
+EXPLAIN
+SELECT 
+    b.id AS booking_id,
+    u.id AS user_id,
+    u.username,
+    p.id AS property_id,
+    p.name AS property_name,
+    pay.id AS payment_id,
+    pay.amount,
+    pay.status
+FROM bookings b
+JOIN users u ON b.user_id = u.id
+JOIN properties p ON b.property_id = p.id
+JOIN payments pay ON b.payment_id = pay.id
+WHERE b.id > 0 AND u.username > ' ';
