442af0c091727d2ede70eecae362cd02ea94633e
EyadZn <84345073+Eyad-Zanaty@users.noreply.github.com>
2025-07-01 19:49:16 +0300

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,8 +1,8 @@
-SELECT airbnb_table.booking, airbnb_table.users from airbnb_table 
+SELECT airbnb_table.booking, airbnb_table.users FROM airbnb_table 
 INNER JOIN result_table ON airbnb_table.booking= result_table.booking;
 
-SELECT airbnb_table.properties, airbnb_table.reviews from airbnb_table 
+SELECT airbnb_table.properties, airbnb_table.reviews FROM airbnb_table 
 LEFT JOIN result_table ON airbnb_table.properties= result_table.properties;
 
-SELECT airbnb_table.users, airbnb_table.booking from airbnb_table 
+SELECT airbnb_table.users, airbnb_table.booking FROM airbnb_table 
 FULL OUTER JOIN result_table ON airbnb_table.users= result_table.users;
