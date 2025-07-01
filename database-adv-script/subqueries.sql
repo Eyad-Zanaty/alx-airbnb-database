1c64a7cba8e6f07888361f3cc0287d76ebc54d56
EyadZn <84345073+Eyad-Zanaty@users.noreply.github.com>
2025-07-01 19:52:23 +0300

    Update subqueries.sql

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,2 +1,2 @@
-SELECT rating from airbnb_table where rating > 4; 
-SELECT booking from airbnb_table where booking > 3; 
+SELECT rating FROM airbnb_table WHERE rating > 4.0 AVG; 
+SELECT booking FROM airbnb_table WHERE booking > 3.0 COUNT; 
